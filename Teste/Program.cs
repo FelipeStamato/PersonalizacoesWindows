d6ea6318b07deb832d4d99fba9d58b58ecac00ba
Felipe Stamato <felipestamato19@gmail.com>
2024-06-24 16:07:06 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Teste.entities;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string caminhoImagem = @"C:\Users\User\Desktop\preto.png";
+        Wallpaper.DefinirWallpaper(caminhoImagem);
+    }
+}
+
+// C:\Users\User\Desktop\branco.png
+// C:\Users\User\Desktop\preto.png
